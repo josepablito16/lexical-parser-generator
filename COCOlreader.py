758697c6635ca053de83d1db48b130e2ce8dff8c
josepablito16 <josepablocif16@gmail.com>
2021-04-03 22:51:57 -0600

    Separacion de secciones CHARACTERS, KEYWORDS, TOKENS V1

--- COCOlreader.py
+++ COCOlreader.py
@@ -1,5 +1,7 @@
 import sys
 
+secciones = ['CHARACTERS', 'KEYWORDS', 'TOKENS', 'PRODUCTIONS']
+
 
 def test():
     '''
@@ -10,13 +12,40 @@ def test():
 
 def getCompilerId(linea):
     '''
-            Revisa si en la linea contiene la palabra reservada COMPILER
-            y extrae el identificador
+                    Revisa si en la linea contiene la palabra reservada COMPILER
+                    y extrae el identificador
     '''
     if (linea[:linea.find(" ")] == "COMPILER" and len(linea[linea.find(" "):]) > 0):
         print(f'''
-			COMPILER
+			IDENTIFICADO <COMPILER>
 			ident = {linea[linea.find(" "):]}
+		''')
+    else:
+        print("ERROR")
+
+
+def getCharactersVerificacion(linea):
+    if linea.strip() == "CHARACTERS":
+        print('''
+			IDENTIFICADO <CHARACTERS>
+		''')
+    else:
+        print("ERROR")
+
+
+def getKeyWordsVerificacion(linea):
+    if linea.strip() == "KEYWORDS":
+        print('''
+			IDENTIFICADO <KEYWORDS>
+		''')
+    else:
+        print("ERROR")
+
+
+def getTokensVerificacion(linea):
+    if linea.strip() == "TOKENS":
+        print('''
+			IDENTIFICADO <TOKENS>
 		''')
     else:
         print("ERROR")
@@ -49,13 +78,7 @@ def eliminarComentarios(lineas):
     comentarioAbierto = False
 
     for linea in lineas:
-        print(f'''
 
-            DEBUG
-            linea = {linea}
-            comentarioAbierto = {comentarioAbierto}
-
-        ''')
         if (linea.find('.)') != -1 and comentarioAbierto):
             comentarioAbierto = False
             continue
@@ -74,15 +97,89 @@ def eliminarComentarios(lineas):
         else:
             listaSinComentarios.append(linea)
 
-    print(listaSinComentarios)
+    return listaSinComentarios
+
+
+def getCharactersSeccion(lista):
+    nuevaLista = []
+    characters = []
+    for i in range(len(lista)):
+        if (lista[i].strip() in secciones):
+            nuevaLista = lista[i:]
+            break
+        characters.append(lista[i])
+
+    print(f'''
+        characters = {characters}
+
+        resto = {nuevaLista}
+    ''')
+
+    return nuevaLista
+
+
+def getKeyWordsSeccion(lista):
+    nuevaLista = []
+    keywords = []
+    for i in range(len(lista)):
+        if (lista[i].strip() in secciones):
+            nuevaLista = lista[i:]
+            break
+        keywords.append(lista[i])
+
+    print(f'''
+        keywords = {keywords}
+
+        resto = {nuevaLista}
+    ''')
+
+    return nuevaLista
+
+
+def getTokensSeccion(lista):
+    nuevaLista = []
+    tokens = []
+    for i in range(len(lista)):
+        if (lista[i].strip() in secciones or lista[i].strip()[0:3] == "END"):
+            nuevaLista = lista[i:]
+            break
+        tokens.append(lista[i])
+
+    print(f'''
+        tokens = {tokens}
+
+        resto = {nuevaLista}
+    ''')
+
+    return nuevaLista
 
 
 if __name__ == "__main__":
 
+    # Limpieza de archivo .atg
+
     listaLimpia = eliminarEspacios(openFile('./test.atg'))
 
-    eliminarComentarios(listaLimpia)
+    listaLimpia = eliminarComentarios(listaLimpia)
+    # print(listaLimpia)
 
-    # getCompilerId(listaLimpia.pop(0))
+    # Identificacion de informacion
+
+    # Identificador de compilador
+    getCompilerId(listaLimpia.pop(0))
+
+    # Characters
+    getCharactersVerificacion(listaLimpia.pop(0))
+    listaLimpia = getCharactersSeccion(listaLimpia)
+
+    # KeyWords
+    getKeyWordsVerificacion(listaLimpia.pop(0))
+    listaLimpia = getKeyWordsSeccion(listaLimpia)
+
+    # Tokens
+    getTokensVerificacion(listaLimpia.pop(0))
+    getTokensSeccion(listaLimpia)
+
+    # print(listaLimpia)
 
     # print(listaLimpia.pop(0))
